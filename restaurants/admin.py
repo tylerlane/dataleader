9ef8bc20bd58eded88b4e0d282ce203591e2da72
Tyler Lane <tyler@nolongervalid.com>
2011-08-01 20:35:31 -0500

    PEP8'ing the admin file.

--- restaurants/admin.py
+++ restaurants/admin.py
@@ -2,20 +2,21 @@ from django.contrib.gis import admin
 from models import *
 
 
-class RestaurantAdmin( admin.OSMGeoAdmin ):
-  field = ( None, { "fields": ("name" ) } ) 
-  field = ( None, { "fields": ("address" ) } )
-  field = ( None, { "geom": ("geom" ) } )
-  list_display = ( "name","address","city" )
-  list_filter = ( "channel","city" )
-  default_lon = 37.214367
-  default_lat = -93.29313
-  order_by = ("name", "address" )
-  search_fields = ("name", )
-class InspectionAdmin( admin.ModelAdmin ):
-  list_display = ("restaurant","date","reinspection",)
-  list_filter = ("critical","noncritical","reinspection",)
-	
-admin.site.register( Restaurant, RestaurantAdmin )
-admin.site.register( Inspection, InspectionAdmin )
+class RestaurantAdmin(admin.OSMGeoAdmin):
+    field = (None, {"fields": ("name")})
+    field = (None, {"fields": ("address")})
+    field = (None, {"geom": ("geom")})
+    list_display = ("name", "address", "city")
+    list_filter = ("channel", "city")
+    default_lon = 37.214367
+    default_lat = -93.29313
+    order_by = ("name", "address")
+    search_fields = ("name", )
 
+
+class InspectionAdmin(admin.ModelAdmin):
+    list_display = ("restaurant", "date", "reinspection",)
+    list_filter = ("critical", "noncritical", "reinspection",)
+
+admin.site.register(Restaurant, RestaurantAdmin)
+admin.site.register(Inspection, InspectionAdmin)
