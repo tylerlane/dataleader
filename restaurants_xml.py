3adee5d1469b4fee0869513c17b5a9040fe7528b
Tyler Lane <tlane2@springfi.gannett.com>
2011-08-10 18:29:01 -0500

    syncing changes

--- restaurants_xml.py
+++ restaurants_xml.py
@@ -3,24 +3,26 @@ soup = BeautifulStoneSoup( open( "/Users/tlane2/Desktop/metromix-export.xml").re
 stuff = [ ]
 items = soup.findAll('content_item')
 for item in items:
-  print "%s" % item.children["name"].contents
+    try:
+        print item.name
+    except:
+        print "error printing name"
+    lat = item.find( 'latitude')
+    if lat is not None:
+        lat = lat.contents
+    lng = item.find( 'longitude')
+    if lng is not None:
+        lng = lng.contents
+    print "lat: %s" % lat
+    print "lng: %s " % lng
+    #attributes = item.findAll( 'attribute' )
+    #for attribute in attributes:
+    #    name = attribute.find('name').contents
+    #    if name not in stuff:
+    #        stuff.append(name)
 
-  lat = item.find( 'latitude')
-  if lat is not None:
-    lat = lat.contents
-  lng = item.find( 'longitude')
-  if lng is not None:
-    lng = lng.contents
-  print "lat: %s" % lat
-  print "lng: %s " % lng
-  attributes = item.findAll( 'attribute' )
-  for attribute in attributes:
-    name = attribute.find('name').contents
-    if name not in stuff:
-      stuff.append(name)
-
-for foo in stuff:
-  print foo
+    #for foo in stuff:
+    #    print foo
 
 
 
